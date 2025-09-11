71ac7db95c2339dd9640fcd50125cca83690efe3
Fr5ctal-Dev <fr5ctal@gmail.com>
2025-09-11 13:44:40 +0800

    Add execute_scene task cleanup

--- editor/tasks/execute_scene.py
+++ editor/tasks/execute_scene.py
@@ -25,9 +25,13 @@ class ExecuteSceneTask(BaseTask):
         self.process = subprocess.Popen([python_executable, self.file], cwd=os.path.dirname(self.file), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         while self.process.poll() is None:
             pass
+        
         if self.process.poll() != 0:
             self.new_error_chunk.emit(self.process.stderr.read())
         self.new_text_chunk.emit('\nExecution ended with exit code ' + str(self.process.poll()))
+
+        self.process.wait()
+        self.temp.cleanup()
 
         self.finished.emit()
 
