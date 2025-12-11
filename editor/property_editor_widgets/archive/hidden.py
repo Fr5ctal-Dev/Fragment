5b3a53726e8de7ca7770a4b0fd987b51cdc13361
Fr5ctal-Dev <fr5ctal@gmail.com>
2025-12-11 12:18:30 +0800

    Remove archived property editor widgets

--- editor/property_editor_widgets/archive/hidden.py
+++
@@ -1,9 +0,0 @@
-from .editor_widget import EditorWidget
-from PySide6.QtWidgets import QLabel
-
-
-class Hidden(EditorWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.label = QLabel('None')
-        self.main_layout.addWidget(self.label)
